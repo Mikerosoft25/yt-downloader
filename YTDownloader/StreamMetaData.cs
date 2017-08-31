4d4ce2bc267ea181e14977b2f11068b62e8ff0e5
Nils Albrecht <nralbrecht@web.de>
2017-08-31 19:44:29 +0200

    Add data classes

---
+++ YTDownloader/StreamMetaData.cs
@@ -0,0 +1,14 @@
+﻿namespace YTDownloader
+{
+    public class StreamMetaData
+    {
+        public int Bitrate { get; private set; }
+        public string Url { get; private set; }
+
+        public StreamMetaData(int bitrate, string url)
+        {
+            Bitrate = bitrate;
+            Url = url;
+        }
+    }
+}
---
+++ YTDownloader/VideoMetaData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YTDownloader
+{
+    class VideoMetaData
+    {
+        public string Id { get; private set; }
+        public string Title { get; private set; }
+        public string ThumbnailUrl { get; private set; }
+        public IEnumerable<StreamMetaData> Streams { get; private set; }
+        
+        public VideoMetaData(string id, string title, string thumbnailUrl, IEnumerable<StreamMetaData> audioStreams)
+        {
+            Id = id;
+            Title = title;
+            ThumbnailUrl = thumbnailUrl;
+            Streams = audioStreams;
+        }
+    }
+}
